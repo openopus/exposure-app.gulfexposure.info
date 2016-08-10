5100fd2261b2e6c34a2093b89330f1e72fe2ebf4
Brian J. Fox <bfox@opuslogica.com>
2016-08-10 15:45:20 -0700

    add missing </div>, allow for fast-forwarding of delays, but still let things fade in nicely, including the bottom button.

--- www/exposure/home/controllers.js
+++ www/exposure/home/controllers.js
@@ -1,4 +1,26 @@
 Controllers.controller('HomeController', function($scope, $location, $state) {
   $scope.go_home2 = function() { $state.go("app.home2"); };
   $scope.go_dashboard = function() { $state.go("app.dashboard"); };
+
+  $scope.accelerate = function() {
+    var raw_elements = document.getElementsByClassName("dramatic-paragraph");
+    angular.forEach(raw_elements, function(raw) {
+      var element = angular.element(raw);
+      element.removeClass("delay-1");
+      element.removeClass("delay-2");
+      element.removeClass("delay-3");
+      element.removeClass("delay-4");
+      element.removeClass("delay-5");
+      element.removeClass("delay-6");
+      element.removeClass("delay-7");
+      element.removeClass("slow-fadein");
+      element.addClass("smooth-fadein");
+    });
+
+    raw_elements = document.getElementsByClassName("delay-7");
+    angular.forEach(raw_elements, function(raw) {
+      var element = angular.element(raw);
+      element.removeClass("delay-7");
+    });
+  }
  });
