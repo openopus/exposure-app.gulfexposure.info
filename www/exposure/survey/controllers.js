700449ef52e8b31f2c1c78cf55954ed3762b85d8
Brian J. Fox <bfox@opuslogica.com>
2016-08-21 11:41:24 -0700

    Better development world

--- www/exposure/survey/controllers.js
+++ www/exposure/survey/controllers.js
@@ -6,23 +6,25 @@ Controllers.controller('SurveyController', function($scope, $transitions, $timeo
   };
 
   $scope.setup = function() {
-    var survey_promise;
-    var groups_promise = Survey.get_survey_template();
+    Survey.initialized.then(function() {
+      var survey_promise;
+      var groups_promise = Survey.get_survey_template();
 
-    if ($stateParams.codename) {
-      survey_promise = Survey.get_survey_by_codename($stateParams.codename);
-    } else {
-      survey_promise = Survey.new_survey();
-    }
+      if ($stateParams.codename) {
+        survey_promise = Survey.get_survey_by_codename($stateParams.codename);
+      } else {
+        survey_promise = Survey.new_survey();
+      }
 
-    $q.all([survey_promise, groups_promise]).then(function(values) {
-      var survey = values[0], groups = values[1];
-      Survey.zipper(groups, survey.answers);
-      var codename_question = Survey.get_question_by_name("Codename");
-      if (codename_question) codename_question.answer = survey.user.codename;
-      ExposureCodename.set_current(survey.user.codename);
-      $scope.survey = survey;
-      $scope.groups = groups;
+      $q.all([survey_promise, groups_promise]).then(function(values) {
+        var survey = values[0], groups = values[1];
+        Survey.zipper(groups, survey.answers);
+        var codename_question = Survey.get_question_by_name("Codename");
+        if (codename_question) codename_question.answer = survey.user.codename;
+        ExposureCodename.set_current(survey.user.codename);
+        $scope.survey = survey;
+        $scope.groups = groups;
+      });
     });
   };
 
