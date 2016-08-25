5404902e4eaf12209f99cf1dc3a037f8569ddc6c
Brian J. Fox <bfox@opuslogica.com>
2016-08-25 14:20:09 -0700

    WHoops

--- www/exposure/services.js
+++ www/exposure/services.js
@@ -1,7 +1,7 @@
 Factories.factory("$api", function($http, $q) {
   var service = {};
   var baseURL = "https://api.gulfexposure.info/api";
-  baseURL = "http://localhost:3000/api";
+  // baseURL = "http://localhost:3000/api";
 
   service.get = function(thing) { return $http.get(baseURL + "/" + thing, service.extra_headers()); };
   service.create = function(thing, data) { return $http.post(baseURL + "/" + thing, data, service.extra_headers()); };
