b4f82ab3ad3f536badae4bae331d60c3090b06c6
Brian J. Fox <bfox@opuslogica.com>
2016-08-30 13:53:43 -0700

    simple work

--- www/exposure/map/controllers.js
+++ www/exposure/map/controllers.js
@@ -1,33 +1,35 @@
 Controllers.controller('MapController', function($scope, $rootScope, $timeout, $api, $timeout, $transitions, NgMap) {
 
   $scope.go_dashboard = function() { $transitions.go("dashboard", { type: "slide", direction: "right" }); };
-
   $scope.mapdata = null;
 
-  NgMap.getMap("exposure-heatmap").then(function(map) {
-    $scope.map = map;
-  });
-
-  try {
-    // $scope.mapdata = $api.get("heatmap");
-  } catch(x) {
-    console.error("Couldn't talk to the API server in map/controllers.js", x);
-  }
-
   $scope.map_setup = function() {
-    var base = document.getElementsByClassName("heatmap-wrapper");
-    var wrapper = angular.element(base);
-    wrapper.css({height: "500px"});
-    console.log("Okay, the map has been initialized.");
+    var heatmap = document.getElementById("exposure-heatmap");
+    var win_height = window.innerHeight;
+    var heatmap_top = heatmap.getBoundingClientRect().top;
+    angular.element(heatmap).css("min-height",  "" + win_height - heatmap_top + "px");
+    google.maps.event.trigger($scope.map, 'resize');
+
+    try {
+      $api.get("heatmap").then(function(response) {
+        $scope.map_points = response.data;
+      });
+    } catch(x) {
+      console.error("Couldn't talk to the API server in map/controllers.js", x);
+    }
+
     var success = function(position) {
       var lat = position.coords.latitude;
       var lng = position.coords.longitude;
       var ctr  = new google.maps.LatLng(lat, lng);
-      console.log("AND, I GOT HERE");
     }
     var failure = function(why) { console.error("Ah, you failed.  Here's why: ", why); };
     navigator.geolocation.getCurrentPosition(success, failure, { timeout: 30000 });
   };
 
-  $scope.map_setup();
+  NgMap.getMap("exposure-heatmap").then(function(map) {
+    $scope.map = map;
+    $scope.map_setup();
+  });
+
 });
