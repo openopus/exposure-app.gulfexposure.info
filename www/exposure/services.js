827988ed4801467b4a9c6dabfa81108b2f2b6cf4
Brian J. Fox <bfox@opuslogica.com>
2016-08-30 14:22:19 -0700

    When on the device, always use the production server if setup to use localhost

--- www/exposure/services.js
+++ www/exposure/services.js
@@ -1,7 +1,11 @@
 Factories.factory("$api", function($http, $q) {
   var service = {};
   var baseURL = "https://api.gulfexposure.info/api";
-  baseURL = "http://localhost:3000/api";
+  // baseURL = "http://localhost:3000/api";
+
+  if (window.cordova && (baseURL == "http://localhost:3000/api")) {
+    baseURL = "https://api.gulfexposure.info/api";
+  };
 
   service.get = function(thing) { return $http.get(baseURL + "/" + thing, service.extra_headers()); };
   service.create = function(thing, data) { return $http.post(baseURL + "/" + thing, data, service.extra_headers()); };
