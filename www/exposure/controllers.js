21896b5d8438606f10f1c37ee39c15ea3866449c
Brian J. Fox <bfox@opuslogica.com>
2016-09-06 13:08:01 -0700

    Assume the best, not the worst.

--- www/exposure/controllers.js
+++ www/exposure/controllers.js
@@ -3,8 +3,8 @@
 // Copyright (c) 2016 Brian J. Fox
 // Author: Brian J. Fox (bfox@opuslogica.com) Tue Sep  6 06:02:21 2016.
 Controllers.controller("appController", function($app, $scope) {
-  $scope.online = false;
-  $scope.network = "none";
+  $scope.online = window.navigator ? window.navigator.onLine : true;;
+  $scope.network = "wifi";
 
   document.addEventListener("online", function(network_type) { $scope.network = network_type.type; $scope.online = true; });
   document.addEventListener("offline", function(network_type) { $scope.network = network_type.type; $scope.online = false; });
