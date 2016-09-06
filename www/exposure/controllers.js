34634b9729b38d30bc9dd0f4241c3b5f31da02c0
Brian J. Fox <bfox@opuslogica.com>
2016-09-06 07:14:31 -0700

    USe the things that work for network connectivity notification

--- www/exposure/controllers.js
+++ www/exposure/controllers.js
@@ -2,10 +2,10 @@
 //
 // Copyright (c) 2016 Brian J. Fox
 // Author: Brian J. Fox (bfox@opuslogica.com) Tue Sep  6 06:02:21 2016.
-Controllers.controller("appController", function($app, $scope, $rootScope, $cordovaNetwork) {
-  $scope.network = $app.network;
-  $scope.online = $app.online;
+Controllers.controller("appController", function($app, $scope) {
+  $scope.online = false;
+  $scope.network = "none";
 
-  $rootScope.$on("$app:network-online", function() { $scope.network = $app.network; $scope.online = $app.online; });
-  $rootScope.$on("$app:network-offline", function() { $scope.network = $app.network; $scope.online = $app.online; });
+  document.addEventListener("online", function(network_type) { $scope.network = network_type.type; $scope.online = true; });
+  document.addEventListener("offline", function(network_type) { $scope.network = network_type.type; $scope.online = false; });
 });
--- www/exposure/services.js
+++ www/exposure/services.js
@@ -2,13 +2,13 @@ Factories.factory("$app", function($rootScope) {
   var service = { network: null, online: null };
 
   document.addEventListener("online", function(network_type) {
-    service.network = network_type;
+    service.network = network_type.type;
     service.online = true;
     $rootScope.$broadcast("$app:network-online");
   });
 
   document.addEventListener("offline", function(network_type) {
-    service.network = network_type;
+    service.network = network_type.type;
     service.online = false;
     $rootScope.$broadcast("$app:network-offline");
   });
